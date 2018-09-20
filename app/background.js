bd901ad59f862ff0f39e2e511e357bd022adb3dd
Daniel Rejon <rejonpardenilla@gmail.com>
2018-09-19 23:11:10 -0500

    Fetch rss feed from nyt

---
+++ app/background.js
@@ -0,0 +1,23 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('installed')
+  chrome.storage.sync.set({color: '#3aa757'}, () => {
+    console.log("The color is green.")
+  })
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      pageManagerRule()
+    ])
+  })
+})
+
+
+var pageManagerRule = () => {
+  return {
+    conditions: [
+      new chrome.declarativeContent.PageStateMatcher(true)
+    ],
+    
+    actions: [ new chrome.declarativeContent.ShowPageAction() ]
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,23 +1,43 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('installed')
-  chrome.storage.sync.set({color: '#3aa757'}, () => {
-    console.log("The color is green.");
-  })
-
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([
-      pageManagerRule()
-    ])
-  })
+document.addEventListener('DOMContentLoaded', () => {
+  getRssFeed('http://rss.nytimes.com/services/xml/rss/nyt/Americas.xml')
 })
 
-
-var pageManagerRule = () => {
-  return {
-    conditions: [
-      new chrome.declarativeContent.PageStateMatcher(true)
-    ],
-    
-    actions: [ new chrome.declarativeContent.ShowPageAction() ]
+var getRssFeed = url => {
+  let xmlhttp = new XMLHttpRequest()
+  xmlhttp.onreadystatechange = function() {
+    let isReady = this.readyState == 4 && this.status == 200
+    if (isReady) {
+      // console.log(this.responseText)
+      showRssFeed(this.responseText)
+    }
   }
+  xmlhttp.open('GET', url)
+  xmlhttp.send()
+}
+
+var showRssFeed = (xmlString) => {
+  parser = new DOMParser()
+  doc = parser.parseFromString(xmlString, 'text/xml')
+  items = doc.querySelectorAll('item')
+  items.forEach(item => {
+    let title = item.querySelector('title').textContent
+    let link = item.querySelector('link').textContent
+    let description = item.querySelector('description').textContent
+
+    pushItemNode(title, description, link)
+  })
+}
+
+var pushItemNode = (title, description, link) => {
+  titleNode = document.createElement('h3')
+  titleNode.textContent = title
+  descriptionNode = document.createElement('p')
+  descriptionNode.textContent = description
+
+  itemNode = document.createElement('div')
+  itemNode.classList.add('item')
+  itemNode.appendChild(titleNode)
+  itemNode.appendChild(descriptionNode)
+
+  document.getElementById('feed').appendChild(itemNode)
 }
